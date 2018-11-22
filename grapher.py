52d32e38ec3e4d39018c5829a9ea53cf4d966cd3
Dmitry Ten <Dmitry.Ten@ibm.com>
2018-11-22 17:14:53 -0500

    Make an alternative CDF grapher

--- grapher.py
+++ grapher.py
@@ -12,3 +12,20 @@ def graph_CDF(data_sets, data_labels, title, xlabel, ylabel):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     plt.show()
+
+def graph_CDF_alt(data_sets, data_labels, title, xlabel, ylabel):
+    fig, ax = plt.subplots(figsize=(8, 4))
+    for i in range(len(data_sets)):
+        total_sum = np.sum(data_sets[i])
+
+        bucket_values = np.sort(np.array(data_sets[i], dtype=np.float32))
+
+        probabilities = bucket_values / total_sum
+        ax.plot(bucket_values, np.cumsum(probabilities), label=data_labels[i])
+
+    ax.grid(True)
+    ax.legend(loc='right')
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.show()
